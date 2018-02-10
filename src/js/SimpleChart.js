3f356fb38e2b718963052cceb680239656e27a9c
Pierre Conti <pierre@tablesdata.com>
2018-02-09 19:27:19 -0800

    Line chart design fix title and margin

--- src/js/SimpleChart.js
+++ src/js/SimpleChart.js
@@ -89,9 +89,48 @@ export default class SimpleChartParameter extends React.Component {
     return { raw: JSONtoCSV(data.rows) };
   }
 
+  displaySettings(){
+    return {
+    	labelRectSize: "0.6em",
+    	labelXMargin: "0.6em",
+    	labelTextMargin: "0.3em",
+    	labelRowHeight: "1.2em",
+    	afterTitle: "1.4em",
+    	afterLegend: "1em",
+    	blockerRectOffset: "0.2em",
+    	lineMarkThreshold: 10, // render marks (dots) on lines if data < N
+    	columnOuterPadding: 0.01, // % of width to pad for columns
+    	columnInnerPadding: 0, // % of col group width to pad btwn each
+    	minPaddingOuter: "1em",
+    	bottomPaddingWithoutFooter: "3em",
+    	yAxisOrient: {
+    		primaryScale: "left",
+    		secondaryScale: "right",
+    	},
+    	aspectRatio: {
+    		wide: (9 / 16),
+    		longSpot: (4 / 3),
+    		smallSpot: (3 / 4)
+    	},
+    	margin: {
+    		top: "2rem",
+    		right: "5rem",
+    		bottom: "5rem",
+    		left: "5rem"
+    	},
+    	padding: {
+    		top: 0,
+    		right: 0,
+    		bottom: 0,
+    		left: 0
+    	}
+    };
+  }
+
   loadChartProps(modelText, data) {
     let model;
     let defaults = chartConfig.xy.defaultProps;
+    chartConfig.xy.display=this.displaySettings();
 
     defaults.chartProps.chartSettings[0].type = this.props.chartType || 'line';
     defaults.chartProps.scale.typeSettings.maxLength = 15;
